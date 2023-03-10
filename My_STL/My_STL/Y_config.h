1f37c511471eb5005343bcab647c5537116c2c2b
ylh <1173012900@qq.com>
2023-03-10 23:05:46 +0800

    STL配置文件

---
+++ My_STL/My_STL/Y_config.h
@@ -0,0 +1,14 @@
+﻿#ifndef Y_CONFIG_H_
+#define Y_CONFIG_H_
+#include <iostream>
+/*
+配置文件
+*/
+
+#define _YLH_BEGIN namespace ylh {
+#define _YLH_END }
+#define USING_YLH using namespace ylh;
+#define THROW_BAD_ALLOC std::cerr<<"Bad alloc!\n"; exit(0);
+
+
+#endif // !Y_CONFIG_H_
